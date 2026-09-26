b01a51d87557f94cc0a91e76d7b76838086b9586
Karthik <dayanakarthik28@gmail.com>
2026-09-26 19:52:40 +0530

    Added password to Customer table

--- app/database/models.py
+++ app/database/models.py
@@ -58,6 +58,7 @@ class Customer(Base):
     id = Column(String, primary_key=True, default=generate_uuid)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    password_hash = Column(String, nullable=False)
     created_at = Column(DateTime, nullable=False, default=utc_now)
 
     orders = relationship("Order", back_populates="customer")
